e742ba1454094bcdeaf5c69d992dca70b289bffd
Vagner Ferreira Santos <cl1470@fiap.com.br>
2017-09-21 15:33:01 -0300

    add picture component

---
+++ src/app/add-picture-button/add-picture-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-add-picture',
+    templateUrl: './add-picture-button.component.html',
+    styleUrls: ['./add-picture-button.component.scss']
+})
+
+export class AddPictureButtonComponent {
+
+}
