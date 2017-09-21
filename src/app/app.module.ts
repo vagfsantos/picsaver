7e9a7af779f24eaccbcd8a09bf79a7f3ed90c190
Vagner Ferreira Santos <cl1470@fiap.com.br>
2017-09-21 15:34:48 -0300

    updating app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
+// angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// libs
 import { MaterializeModule } from 'angular2-materialize';
 
+// components
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './header/header.component';
+import { AddPictureButtonComponent } from './add-picture-button/add-picture-button.component';
+
+// modules
+import { MainContentModule } from './main-content/main-content.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AddPictureButtonComponent
   ],
   imports: [
     MaterializeModule,
-    BrowserModule
+    BrowserModule,
+    MainContentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
